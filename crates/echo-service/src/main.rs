d9a7afd0111b7c602cb5fd957649ba3d34123150
David <david.campion@walmartlabs.com>
2022-08-30 21:03:27 -0700

    fun stuff

---
+++ crates/echo-service/src/main.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use tokio::net::{TcpListener, TcpStream};
+use std::io;
+use std::net::{SocketAddr};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, copy};
+
+
+async fn echo(socket: &mut TcpStream, address: SocketAddr) -> io::Result<()> {
+    println!("Connection Open: {}", address);
+    let (mut reader, mut writer)  = socket.split();
+
+    copy(&mut reader, &mut writer).await?;
+
+    println!("Connection Closed: {}", address);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        match listener.accept().await {
+            Ok((mut socket, addr)) => {
+                tokio::spawn(async move { echo(&mut socket, addr).await });
+            },
+            Err(e) => println!("couldn't get client: {:?}", e),
+        }
+    }
+}
---
+++ crates/http-utils/src/lib.rs
@@ -0,0 +1,39 @@
+// https://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html
+
+struct Status {
+    version: String,
+    code: u8,
+    reason: String
+}
+
+impl From<Status> for String {
+    fn from(status: Status) -> Self {
+        format!("{} {} {}", status.version, status.code, status.reason)
+    }
+}
+
+struct Response {
+    status: Status,
+    body: String
+}
+
+impl From<Response> for String {
+    fn from(response: Response) -> String {
+        format!("{}\n\n{}", String::from(response.status), response.body)
+    }
+}
+
+impl Response {
+    pub fn into_bytes(self) -> Vec<u8> {
+        let response_string = String::from(self);
+
+        response_string.into_bytes()
+    }
+}
+
+impl From<Response> for Vec<u8> {
+    fn from(response: Response) -> Vec<u8> {
+        let x: String = response.into();
+        x.into_bytes()
+    }
+}
